157d60eb24649a646d9c3cdaf5e82ed0c183ea92
XanderG2 <XanderG2@users.noreply.github.com>
2024-01-12 19:16:14 +0000

    ball collision

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ py = height-50-ph//2
 
 paddle = pygame.Rect(px, py, pw, ph)
 
+points = 0
+
 blocks = []
 for x in range(5, width, 60):
     for y in range(5, height-250, 60):
         blocks.append(pygame.Rect(x, y, 50, 50))
 
+mx, my = 0, 0.1
 ball = pygame.Rect(width//2-20, py-100, 40, 40)
+ballx = ball.centerx
+bally = ball.centery
 
 running = True
 while running:
@@ -37,6 +42,26 @@ while running:
             px += 0.2
     paddle.x = round(px)
     paddle.y = round(py)
+    if ball.colliderect(paddle):
+        dfm = ball.centerx - paddle.centerx
+        mx = dfm//45
+        if mx > 10:
+            mx = 10
+        my = -my
+    if ball.top <= 0:
+        my = -my
+    if ball.bottom >= height:
+        points += 1
+        ballx = width//2
+        bally = py-100
+        mx = 0
+        my = 1
+    if ball.left <= 0 or ball.right >= width:
+        mx = -mx
+    ballx += mx
+    bally += my
+    ball.y = round(bally)
+    ball.x = round(ballx)
     screen.fill(black)
     pygame.draw.rect(screen, white, paddle)
     for block in blocks:
